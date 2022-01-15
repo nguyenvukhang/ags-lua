099c0bd4876e247144833428edcc6ff65542187b
nguyenvukhang <brew4k@gmail.com>
2022-01-15 22:52:30 +0800

    check if .git file/folder exists

--- c/main.c
+++ c/main.c
@@ -32,17 +32,32 @@ int main() {
     "~/repos/uni",
     "~/sunnus/app/dev",
     "~/sunnus/web/dev",
+    "~/dummy/meme/lord",
   };
 
   char *HOME = getenv("HOME");
-  printf("%s\n", HOME);
-  char *a;
-  char b[50];
+  // printf("%s\n", HOME);
+  char git_suffix[5] = "/.git";
+  char *no_tildy;
+  char full_path[100];
+  char git_data_file[105];
   for (int i = 0; i < sizeof(scanlist)/sizeof(scanlist[0]); i++) {
-    a = strremove(scanlist[i], "~");
-    strcpy(b, HOME);
-    strcat(b, a);
-    printf("%s\n", b);
+    no_tildy = strremove(scanlist[i], "~");
+
+    strcpy(full_path, HOME);
+    strcpy(git_data_file, HOME);
+
+    strcat(full_path, no_tildy); // b -> full path to repo
+    strcat(git_data_file, no_tildy);
+    strcat(git_data_file, git_suffix);
+
+    if (access(git_data_file, F_OK) == 0) {
+      // file exists
+      printf("    exists");
+    } else {
+      printf("not exists");
+    }
+    printf("%s\n", git_data_file);
   }
 
   /* check if a file exists
