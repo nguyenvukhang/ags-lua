70d91ce8baa227018224d59a88383fb634fdb48b
nguyenvukhang <brew4k@gmail.com>
2022-01-15 21:59:25 +0800

    stashed file reader aside, starting from scratch

--- c/main.c
+++ c/main.c
@@ -1,27 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int main() {
+  char scanlist[100][100] = {"hello"};
 
-int main( int argc, char *argv[] )
-{
-
-  FILE *fp;
-  char path[1035];
-
-  /* Open the command for reading. */
-  fp = popen("/bin/ls /etc/", "r");
-  if (fp == NULL) {
-    printf("Failed to run command\n" );
-    exit(1);
-  }
-
-  /* Read the output a line at a time - output it. */
-  while (fgets(path, sizeof(path), fp) != NULL) {
-    printf("%s", path);
-  }
-
-  /* close */
-  pclose(fp);
 
   return 0;
 }
