1febc89e3611e2cdc31793e51659be879342c0ce
nguyenvukhang <brew4k@gmail.com>
2022-01-15 22:16:06 +0800

    arrays of strings

--- c/main.c
+++ c/main.c
@@ -1,9 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main() {
-  char scanlist[100][100] = {"hello"};
+  char scanlist[][100] = {
+    "~/dots",
+    "~/dots/nvim/.config/nvim",
+    "~/dots/pass/.password-store",
+    "~/dots/personal",
+    "~/dots/walls",
+    "~/dots/zsh",
+    "~/repos/blog",
+    "~/repos/hcanoe-firebase/repo",
+    "~/repos/min",
+    "~/repos/notes",
+    "~/repos/uni",
+    "~/sunnus/app/dev",
+    "~/sunnus/web/dev",
+  };
 
+  /* 
+   * print a particular index
+   */
+  /* printf("%s ", scanlist[0]); */
+
+  /* 
+   * replace a particular index with a new string
+   */
+  /* strcpy(scanlist[0], "Tesla"); */
+
+  // length of array of strings
+  for (int i = 0; i < sizeof(scanlist)/sizeof(scanlist[0]); i++) {
+    printf("%s\n", scanlist[i]);
+  }
 
   return 0;
 }
