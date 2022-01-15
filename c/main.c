d5b68cdd7e86bd368a7f001e472ad4464b184841
nguyenvukhang <brew4k@gmail.com>
2022-01-15 22:43:22 +0800

    iterate over scanlist, print tildy path

--- c/main.c
+++ c/main.c
@@ -3,6 +3,20 @@
 #include <string.h>
 #include <unistd.h>
 
+char *strremove(char *str, const char *sub) {
+  char *p, *q, *r;
+  if (*sub && (q = r = strstr(str, sub)) != NULL) {
+    size_t len = strlen(sub);
+    while ((r = strstr(p = r + len, sub)) != NULL) {
+      while (p < r)
+        *q++ = *p++;
+    }
+    while ((*q++ = *p++) != '\0')
+      continue;
+  }
+  return str;
+}
+
 int main() {
   char scanlist[][100] = {
     "~/dots",
@@ -22,6 +36,14 @@ int main() {
 
   char *HOME = getenv("HOME");
   printf("%s\n", HOME);
+  char *a;
+  char b[50];
+  for (int i = 0; i < sizeof(scanlist)/sizeof(scanlist[0]); i++) {
+    a = strremove(scanlist[i], "~");
+    strcpy(b, HOME);
+    strcat(b, a);
+    printf("%s\n", b);
+  }
 
   /* check if a file exists
    * https://stackoverflow.com/questions/230062/whats-the-best-way-to-check-if-a-file-exists-in-c
@@ -39,9 +61,12 @@ int main() {
   /* strcpy(scanlist[0], "Tesla"); */
 
   // length of array of strings
-  for (int i = 0; i < sizeof(scanlist)/sizeof(scanlist[0]); i++) {
-    printf("%s\n", scanlist[i]);
-  }
+
+  /* for (int i = 0; i < sizeof(scanlist)/sizeof(scanlist[0]); i++) {
+   *   printf("%s\n", scanlist[i]);
+   * }
+   *
+   * */
 
   return 0;
 }
