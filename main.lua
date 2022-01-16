51215107362fb60dbf6506294e2f62aec749d73a
nguyenvukhang <brew4k@gmail.com>
2022-01-17 00:14:39 +0800

    shortened

--- main.lua
+++ main.lua
@@ -1,13 +1,9 @@
 #!/usr/bin/env lua
 
-function os.capture(cmd, raw)
+function os.capture(cmd)
   local f = assert(io.popen(cmd, 'r'))
   local s = assert(f:read('*a'))
   f:close()
-  if raw then return s end
-  s = string.gsub(s, '^%s+', '')
-  s = string.gsub(s, '%s+$', '')
-  s = string.gsub(s, '[\n\r]+', ' ')
   return s
 end
 
@@ -30,7 +26,6 @@ utils.exists = function (file)
       return true
     end
   end
-  -- return ok, err
   return ok
 end
 
@@ -57,13 +52,27 @@ utils.split = function (inputstr, sep)
   return t
 end
 
-utils.make_dict = function(t)
-  local r = {}
-  for _, v in pairs(t) do r[v] = true end
-  return r
+utils.cmd_get = function (cmd, t)
+  local table = t or false
+  local r = os.capture(cmd)
+  if r == nil or r == '' then
+    return nil
+  else
+    if table then
+      return utils.split(r, '\n\r')
+    else -- return string
+      return r:gsub('[\r\n]$', '')
+    end
+  end
 end
 
-local u = utils
+utils.parse_commands = function (arg)
+  local a = {}
+  for _, v in pairs(arg) do a[v] = true end
+  return {
+    list = a["list"] or a["ls"],
+  }
+end
 
 local colors = {}
 
@@ -75,38 +84,18 @@ colors.purple = function(t) return '\27[35m'..t..'\27[0m' end
 colors.cyan   = function(t) return '\27[36m'..t..'\27[0m' end
 colors.gray   = function(t) return '\27[37m'..t..'\27[0m' end
 
-local c = colors
-
 local git = {}
 
-local cmd_get_table = function (cmd)
-  local r = os.capture(cmd, true)
-  if r == nil or r == '' then
-    return nil
-  else
-    return u.split(r, '\n\r')
-  end
-end
-
-local cmd_get_string = function (cmd)
-  local r = os.capture(cmd, true)
-  if r == nil or r == '' then
-    return nil
-  else
-    return r:gsub('[\r\n]$', '')
-  end
-end
-
 git.status = function (dir)
-  local t = cmd_get_table("git -C "..dir.." status --short")
+  local t = utils.cmd_get("git -C "..dir.." status --short", true)
   if t == nil then return nil end
   local r = {}
   for _, i in pairs(t) do
     local _, _, tag = string.find(i, "^(..)")
     if string.sub(tag, 0, 1) == " " then
-      i = c.red(i)
+      i = colors.red(i)
     else
-      i = c.green(i)
+      i = colors.green(i)
     end
     table.insert(r, i)
   end
@@ -114,24 +103,24 @@ git.status = function (dir)
 end
 
 git.cherry = function (dir)
-  local t =  cmd_get_table("git -C "..dir.." cherry -v --abbrev")
+  local t =  utils.cmd_get("git -C "..dir.." cherry -v --abbrev", true)
   if t == nil then return nil end
   local r = {}
   for _, i in pairs(t) do
     local _, _, _, id, msg = string.find(i, "^(%p) (%w+) (.*)$")
-    id = c.yellow(id)
+    id = colors.yellow(id)
     table.insert(r, id..' '..msg)
   end
   return r
 end
 
 git.branch = function (dir)
-  return cmd_get_string("git -C "..dir.." branch --show-current")
+  return utils.cmd_get("git -C "..dir.." branch --show-current")
 end
 
 git.remote = function (dir, upstream)
   local origin = upstream or 'origin'
-  local r = cmd_get_string("git -C "..dir.." config --get remote."..origin..".url")
+  local r = utils.cmd_get("git -C "..dir.." config --get remote."..origin..".url")
   -- lua regex: https://www.lua.org/pil/20.2.html
   r = r:gsub('^https://.*/(.*)/', '%1/')
   r = r:gsub('^git@.*:', '')
@@ -153,50 +142,27 @@ git.summary = function (dir)
 end
 
 local Repo = {}
-Repo.mt = {}
 
 function Repo.new (dir)
   local repo = git.summary(dir)
   if repo.commits or repo.status then
-    setmetatable(repo, Repo.mt)
     return repo
   else
     return nil
   end
 end
 
-function Repo.tostring (set)
-  local s = "{"
-  local sep = ""
-  for k, e in pairs(set) do
-    s = s .. sep .. k .. " = " .. e
-    sep = ", "
-  end
-  return s .. "}"
-end
-
-function Repo.print (set)
-  print(Repo.tostring(set))
-end
-
 local env = {}
 env.home = os.getenv("HOME")
 
-local parse_commands = function (arg)
-  local a = u.make_dict(arg)
-  return {
-    list   = a["list"]   or a["ls"],
-  }
-end
-
-local scanlist = u.lines_from(env.home..'/.config/ags/scanlist')
+local scanlist = utils.lines_from(env.home..'/.config/ags/scanlist')
 
 local git_repos = {}
 local not_repos = {}
 for _, k in pairs(scanlist) do
   local dir = k:gsub('^~', env.home)
   local g = dir .. '/.git'
-  if u.exists(g) then
+  if utils.exists(g) then
     table.insert(git_repos, dir)
   else
     table.insert(not_repos, dir)
@@ -211,29 +177,27 @@ for _, dir in pairs(git_repos) do
   end
 end
 
-local list = {}
-
 local verify_git = function (sl)
   local ok = {}
   for _, dir in pairs(sl) do
     local full_path = dir:gsub('^~', env.home)
     local g = full_path .. '/.git'
-    if u.exists(g) then
+    if utils.exists(g) then
       ok[dir] = true
     end
   end
   return ok
 end
 
-list.main = function (sl)
+local list = function (sl)
   table.sort(sl)
   print('\ncurrent scanlist:')
   local ok = verify_git(sl)
   for _, v in pairs(sl) do
     if ok[v] then
-      print(c.green(v))
+      print(colors.green(v))
     else
-      print(c.red(v))
+      print(colors.red(v))
     end
   end
   print()
@@ -241,18 +205,18 @@ end
 
 local main = function ()
   for _, r in pairs(repos) do
-    local name = c.green(r.name)
-    print('\n'..c.gray('[')..name..c.gray('/'..r.branch..']'))
+    local name = colors.green(r.name)
+    print('\n'..colors.gray('[')..name..colors.gray('/'..r.branch..']'))
     table.print(r.commits)
     table.print(r.status)
   end
   print()
 end
 
-local commands = parse_commands(arg)
+local commands = utils.parse_commands(arg)
 
 if commands.list then
-  list.main(scanlist)
+  list(scanlist)
 else
   main()
 end
